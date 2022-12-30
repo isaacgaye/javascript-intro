ce00dc6c8b43e92bbcec6b5f73df69c6c9c61934
Isaac Gaye <isaacgaye@gmail.com>
2022-12-30 17:46:09 +0100

    Create 01xx-introtopics.js

---
+++ 01xx-introtopics.js
@@ -0,0 +1,28 @@
+//Data types
+
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+//Arithmetic operations
+console.log(37 + 3.5)
+console.log(2022 - 1969)
+console.log(65/240)
+console.log(0.2708 * 100)
+
+//Concatenation
+console.log('Hello' + 'World');
+console.log('Hello'+' '+'World');
+
+//Properties
+console.log('Teaching the world how to code'.length); // Prints 30
+
+//Methods
+console.log('Codecademy'.toUpperCase()); // Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('    Remove whitespace   '.trim()); // Use a string method to log the following string without whitespace at the beginning and end of it.
+
+//Other objects than console
+console.log(Math.floor(Math.random()*100))
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
